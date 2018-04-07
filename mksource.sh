8a9ab4210d4ffe2d1edbb16d39a3af83d05454a7
shylock <tcath2s@gmail.com>
2018-04-07 21:39:36 +0800

    specify template by absolute path

--- mksource.sh
+++ mksource.sh
@@ -16,18 +16,23 @@ do
 			pattern_header='\.h$'
 			pattern_source='\.c$'
 
-			if [[ $OPTARG =~ $pattern_header ]]; then
-				#uppername=$( sed -e 's/[^_a-zA-Z][^_a-zA-Z0-9]{0,30}/_/g' <<< $OPTARG )
-				uppername=$( sed -e 's/\.h$//g' -e 's/\([a-z]\+\)/\U\1/g' <<< $OPTARG )
-				cp ./template/template.h $dir/$OPTARG
-				sed -i  -e "s/\\date.\+/\\date $today/g" \
-					-e "s/_.\+_H_/_${uppername}_H_/g" $dir/$OPTARG
-		
-			elif [[ $OPTARG =~ $pattern_source ]]; then
-				cp ./template/template.c $dir/$OPTARG
-				sed -i "s/\\date.\+/\\date $today/g" $dir/$OPTARG
-			else
-				echo '[err]:Unkown file type!'
+			if [ -e ~/Workspace/config.linux/template/template.h ]; then
+
+				if [[ $OPTARG =~ $pattern_header ]]; then
+					#uppername=$( sed -e 's/[^_a-zA-Z][^_a-zA-Z0-9]{0,30}/_/g' <<< $OPTARG )
+					uppername=$( sed -e 's/\.h$//g' -e 's/\([a-z]\+\)/\U\1/g' <<< $OPTARG )
+					cp ~/Workspace/config.linux/template/template.h $dir/$OPTARG
+					sed -i  -e "s/\\date.\+/\\date $today/g" \
+						-e "s/_.\+_H_/_${uppername}_H_/g" $dir/$OPTARG
+			
+				elif [[ $OPTARG =~ $pattern_source ]]; then
+					cp ~/Workspace/config.linux/template/template.c $dir/$OPTARG
+					sed -i "s/\\date.\+/\\date $today/g" $dir/$OPTARG
+				else
+					echo '[err]:Unkown file type!'
+				fi
+			else 
+				echo "Err:can't find source template file!"
 			fi
 			;;
 		h)
