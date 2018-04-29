bd691c90a031a4f3e0b6c1443f56f7aa2d9db7e2
shylock <tcath2s@gmail.com>
2018-04-29 13:01:19 +0800

    add Makefile template

--- mksource.sh
+++ mksource.sh
@@ -4,40 +4,42 @@
 
 today=$(date +%Y-%m-%d)
 
-dir=''
+prefix="$HOME/Workspace/config.linux/template"
 
-while getopts 'o:n:h' args
+while getopts 'n:h' args
 do
 	case $args in
-		o)  #!< directory to create source file
-			dir=$OPTARG
-			;;
 		n)  #!< name of source file
 			pattern_header='\.h$'
 			pattern_source='\.c$'
+				
+			# Makefile
+			#if [ $OPTARG == 'Makefile' ] && [ -e $prefix/Makefile ]; then
+			if [ $OPTARG == 'Makefile' ]; then
+				cp $prefix/Makefile $PWD/$OPTARG
 
-			if [ -e ~/Workspace/config.linux/template/template.h ]; then
-
-				if [[ $OPTARG =~ $pattern_header ]]; then
-					#uppername=$( sed -e 's/[^_a-zA-Z][^_a-zA-Z0-9]{0,30}/_/g' <<< $OPTARG )
-					uppername=$( sed -e 's/\.h$//g' -e 's/\([a-z]\+\)/\U\1/g' <<< $OPTARG )
-					cp ~/Workspace/config.linux/template/template.h $dir/$OPTARG
-					sed -i  -e "s/\\date.\+/\\date $today/g" \
-						-e "s/_.\+_H_/_${uppername}_H_/g" $dir/$OPTARG
-			
-				elif [[ $OPTARG =~ $pattern_source ]]; then
-					cp ~/Workspace/config.linux/template/template.c $dir/$OPTARG
-					sed -i "s/\\date.\+/\\date $today/g" $dir/$OPTARG
-				else
-					echo '[err]:Unkown file type!'
-				fi
-			else 
-				echo "Err:can't find source template file!"
+			# header file
+			#elif [[ $OPTARG =~ $pattern_header ]] && [ -e $prefix/template.h ]; then
+			elif [[ $OPTARG =~ $pattern_header ]]; then
+				#uppername=$( sed -e 's/[^_a-zA-Z][^_a-zA-Z0-9]{0,30}/_/g' <<< $OPTARG )
+				uppername=$( sed -e 's/\.h$//g' -e 's/\([a-z]\+\)/\U\1/g' <<< $OPTARG )
+				cp $prefix/template.h $PWD/$OPTARG
+				sed -i  -e "s/\\date.\+/\\date $today/g" \
+					-e "s/_.\+_H_/_${uppername}_H_/g" $PWD/$OPTARG
+		
+			# source file
+			#elif [[ $OPTARG =~ $pattern_source ]] && [ -e $prefix/template.c ]; then
+			elif [[ $OPTARG =~ $pattern_source ]]; then
+				cp $prefix/template.c $PWD/$OPTARG
+				sed -i "s/\\date.\+/\\date $today/g" $PWD/$OPTARG
+			else
+				echo '[err]:Unkown file type!'
 			fi
+
 			;;
 		h)
-			echo "usage: $0 -o <output directory> -n <name-of-source>\n\
-				eg.$0 -o ~/Workspace/example/ -n example.h"
+			echo "usage: $0 -n <name-of-source>\n\
+				eg.$0 -n example.h"
 			;;
 		?)
 			echo "[err]:Unknown argument $OPTARG!"
