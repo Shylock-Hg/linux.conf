5950f530d7ee0da891a9131dd091395f1fb20e8c
shylock <tcath2s@gmail.com>
2018-06-12 03:58:16 +0000

    python version switch bash script

---
+++ archlinux-python.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+
+################################################################################
+#switch python2 or python3 to python
+################################################################################
+
+
+while getopts 'p:h' args
+do
+	case $args in
+		p)  #!< name of source file
+
+			if [ -e /usr/sbin/$OPTARG ]; then 
+				sudo mv /usr/sbin/python /usr/sbin/python.bak
+				sudo ln -s /usr/sbin/$OPTARG /usr/sbin/python
+				echo "Change python to /usr/sbin/$OPTARG!"
+			else
+				ehco "/usr/sbin/$OPTART:no such file or directory!"
+			fi
+
+			;;
+		h)
+			echo "usage: $0 -n <python>\n\
+				eg.$0 -p python2"
+			;;
+		?)
+			echo "[err]:Unknown argument $OPTARG!"
+			;;
+	esac
+done
+
+shift $(($OPTIND - 1))
+
+
