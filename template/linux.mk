5608a3688ce231709299aecb648b62e82111d16d
Shylock Hg <tcath2s@gmail.com>
2018-08-17 11:03:01 +0800

    Update linux comman Makefile

--- template/linux.mk
+++ template/linux.mk
@@ -1,35 +1,41 @@
+CC = gcc
+
 CFLAGS = -std=c99 -Wall
 
 DIR_BUILD = ./build
 
 PPFLAGS = -MT $@ -MMD -MP -MF $(DIR_BUILD)/$*.d
 
-SOURCES = $(wildcard *.c)
+SOURCES = 
 
-OBJS = $(addprefix $(DIR_BUILD)/, $(patsubst %.c, %.o, $(notdir $(SOURCES))))
+OBJECTS = $(addprefix $(DIR_BUILD)/, $(patsubst %.c, %.o, $(notdir $(SOURCES))))
 
 TARGET = test
 
-DEPFILES = $(patsubst %.o, %.d, $(OBJS))
+DEPFILES = $(patsubst %.o, %.d, $(OBJECTS))
 
+# set c sources search path
+vpath %.c $(sort $(dir $(SOURCES)))
 
 .PHONY : all clean
 all : $(DIR_BUILD)/$(TARGET)
 
-$(DIR_BUILD)/$(TARGET) : $(OBJS) $(DIR_BUILD)
-	cc $(CFLAGS) -o $@ $(OBJS)
+$(DIR_BUILD)/$(TARGET) : $(OBJECTS) Makefile
+	$(CC) $(CFLAGS) -o $@ $(OBJECTS)
 
-$(DIR_BUILD)/%.o : %.c $(DIR_BUILD)/%.d $(DIR_BUILD)
-	cc $(PPFLAGS) $(CFLAGS) -c $< -o $@
+$(DIR_BUILD)/%.o : %.c Makefile | $(DIR_BUILD)
+	$(CC) $(PPFLAGS) $(CFLAGS) -c $< -o $@
+
+#$(OBJECTS): $(DIR_BUILD)/%.o: %.c
+	#mkdir -p $(@D)
+	#$(CC) $(PPFLAGS) $(CFLAGS) -c $< -o $@
 
 $(DIR_BUILD)/%.d : ;
 .PRECIOUS : $(DIR_BUILD)/%.d
 
-#create build directory
 $(DIR_BUILD) : 
 	mkdir -p $(DIR_BUILD)
 
-#clean outputs
 clean : 
 	rm -rf $(DIR_BUILD)
 
