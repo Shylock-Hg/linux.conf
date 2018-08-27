4f1ce22b18f4cca2c1e8bf46a1ac053cd4789011
shylock <tcath2s@gmail.com>
2018-08-27 21:32:15 +0800

    Add recipe of reStructuredText and c lint for ale

--- viman/recipe-ale.sh
+++ viman/recipe-ale.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env sh
 
+NATIVE_INSTALL='pacman -S'
+AUR_INSTALL='aurman -S'
+
 # build vim help documents
 echo '" Load all plugins now.
 " Plugins need to be added to runtimepath before helptags can be generated.
@@ -12,8 +15,20 @@ silent! helptags ALL' >> $HOME/.vimrc
 pip install --user autopep8 black flake8 isort mypy prospector pycodestyle 'python-language-server[all]' pyre-check pylint vulture yapf
 
 # support for markdown
-sudo npm install alex prettier remark-lint textlint markdownlint write-good 
-gem install mdl 
+#mkdir temp && cd temp && curl -L https://github.com/errata-ai/vale/releases/download/v1.0.1/vale_1.0.1_Linux_64-bit.tar.gz | tar -zxvf - && sudo cp vale /usr/bin/ && cd .. && rm -rf ./temp
+sudo npm install alex prettier remark-lint textlint markdownlint write-good
+gem install mdl
 pip install --user proselint pyredpen
-wget https://github.com/errata-ai/vale/releases/download/v1.0.1/vale_1.0.1_Linux_64-bit.tar.gz | tar -xvf vale_1.0.1_Linux_64-bit.tar.gz && sudo cp vale /usr/bin/
+
+# support for reStructuredText
+#$(AUR_INSTALL) vale
+#mkdir temp && cd temp && curl -L https://github.com/errata-ai/vale/releases/download/v1.0.1/vale_1.0.1_Linux_64-bit.tar.gz | tar -zxvf - && sudo cp vale /usr/bin/ && cd .. && rm -rf ./temp
+sudo npm install alex write-good
+pip install --user proselint pyredpen rstcheck
+
+# support for c
+$(NATIVE_INSTALL) cppcheck clang gcc
+$(AUR_INSTALL) cquery
+pip install --user cpplint flawfinder
+
 
