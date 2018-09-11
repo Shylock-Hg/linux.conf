4f9849893592ecd88d613fb8608c9c000a0cba64
Shylock Hg <tcath2s@gmail.com>
2018-09-11 15:29:21 +0800

    Reorganize shell script by key definition

--- sync.sh
+++ sync.sh
@@ -1,58 +1,65 @@
 #! /usr/bin/env bash
 
+VIM_RC=("$HOME/.vimrc" './.vimrc')
+VIM_MAN=("$HOME/.viman.yml" './viman/.viman.yml')
+
+SS_PAC=("$HOME/.ss.pac" './.ss.d/.ss.pac')
+SS_CONF=('/etc/shadowsocks/.ss.conf.json' './.ss.d/.ss.conf.json')
+
 while getopts 'o:h' args
 do
 	case $args in
 		o)  #orientation
-                        if [ $OPTARG = "backup" ]
-                        then
-                                #vim
-                                cp ~/.vimrc                       ./
-								cp ~/.viman.yml                   ./viman
-				#cp ~/.vimpkg/vim_config.json      ./
-                                #shadowsocks
-                                #if [ ! -e ./.ss.d/ ]
-                                #then 
-                                #        mkdir ./.ss.d
-                                #fi
-								mkdir -p                          ./.ss.d
-                                cp ~/.ss.pac                      ./.ss.d/
-                                cp /etc/shadowsocks/.ss.conf.json ./.ss.d/
-                                #conda
-                                #if [ ! -e .conda ]
-                                #then
-                                #        mkdir .conda
-                                #fi
-								#mkdir -p                          .conda
-                                #source activate mltoolchain
-                                #conda env export -f               ./.conda/mltoolchain.yml
-                                #source deactivate
-                                echo "Backup ok!"
+			if [ $OPTARG = "backup" ]
+			then
+				#vim
+				if [ -e ${VIM_RC[0]} ] ; then
+					cp ${VIM_RC[0]}  ${VIM_RC[1]}
+				else 
+					echo "error : ${VIM_RC[0]} do not exist!"
+				fi
+				if [ -e ${VIM_MAN[0]} ] ; then
+					cp ${VIM_MAN[0]} ${VIM_MAN[1]}
+				else 
+					echo "error : ${VIM_MAN[0]} do not exist!"
+				fi
 
-                        elif [ $OPTARG = "reduct" ]
-                        then
-                                #vim
-                                cp .vimrc              ~/
-								cp viman/.viman.yml          ~/
-                                #shadowsocks
-                                cp .ss.d/.ss.pac       ~/
-                                if [ -e /etc/shadowsocks/ ] 
-                                then
-                                        cp .ss.d/.ss.conf.json /etc/shadowsocks/
-                                else
-                                        echo "null /ect/shadowsocks!"
-                                fi
-                                echo "Reduct ok!"
+				#shadowsocks
+				if [ -e ${SS_PAC[0]} ] ; then
+					cp  ${SS_PAC[0]}  ${SS_PAC[1]}
+				else
+					echo "error : ${SS_PAC[0]} do not exist!"
+				fi
+				if [ -e ${SS_CONF[0]} ] ; then
+					cp ${SS_CONF[0]} ${SS_CONF[1]}
+				else
+					echo "error : ${SS_CONF[0]} do not exist!"
+				fi
 
-                        else
-                                echo "Unknown argument $OPTARG"
-                        fi
+			elif [ $OPTARG = "reduct" ]
+			then
+				#vim
+				cp ${VIM_RC[1]}  ${VIM_RC[0]}
+				cp ${VIM_MAN[1]} ${VIM_MAN[0]}
+
+				#shadowsocks
+				cp ${SS_PAC[1]} ${SS_PAC[0]}
+				if [ -e $(dirname ${SS_CONF[0]}) ] 
+				then
+					sudo cp ${SS_CONF[1]} ${SS_CONF[0]}
+				else
+					echo "error : $(dirname ${SS_CONF[1]}) do not exist!"
+				fi
+
+			else
+				echo "error : Unknown argument $OPTARG"
+			fi
 			;;
-                h)
-                        echo "Usage: ./sync.sh -o <backup or reduct>!"
-                        ;;
+		h)
+			echo "Usage: ./sync.sh -o <backup or reduct>!"
+			;;
 		?)
-			echo "Unknown parameter!"
+			echo "error : Unknown parameter!"
 			;;
 	esac
 done
