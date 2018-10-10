49549461f349c3be776c6b18b1c0764a383a20db
Shylock Hg <tcath2s@gmail.com>
2018-10-10 16:03:11 +0800

    Add --needed option for installation of powerline-fonts in airline plugin recipe

--- viman/recipe-vim-airline.sh
+++ viman/recipe-vim-airline.sh
@@ -1,2 +1,2 @@
-sudo pacman -Sy --noconfirm powerline-fonts
+sudo pacman --needed --noconfirm -Sy powerline-fonts
 echo -e "\n\"airline\nlet g:airline_solarized_bg='dark'\nlet g:airline_powerline_fonts = 1" >> $HOME/.vimrc
