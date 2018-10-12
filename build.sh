106ef40fd98f9712d0f6bbe5de95eb2df88e7fef
Shylock-Hg <tcath2s@gmail.com>
2018-10-12 03:47:59 +0000

    Install my custom ultility to /usr/local/bin by shell script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env sh
+
+###############################################################################
+# install my custom ultility scripts to `/usr/bin`
+# 1. archlinux-python
+# 2. mksource
+# 3. win2nix
+###############################################################################
+
+# archlinux-python
+sudo curl -sL https://raw.githubusercontent.com/Shylock-Hg/config.linux/master/archlinux-python.sh -o /usr/local/bin/archlinux-python
+sudo chmod a+x /usr/local/bin/archlinux-python
+
+# mksource
+sudo curl -sL https://raw.githubusercontent.com/Shylock-Hg/config.linux/master/mksource.sh -o /usr/local/bin/mksource
+sudo chmod a+x /usr/local/bin/mksource
+
+# win2nix
+sudo curl -sL https://raw.githubusercontent.com/Shylock-Hg/config.linux/master/win2nix.sh -o /usr/local/bin/win2nix
+sudo chmod a+x /usr/local/bin/win2nix
+
