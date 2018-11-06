a211aab90c5b3ac3aa43ce158291d0996e0058e7
Shylock-Hg <tcath2s@gmail.com>
2018-11-06 07:13:15 +0000

    Revert "Disable dev docker image CI/CD"
    This reverts commit d8de92fabb0d861cb8695d4c66f6c42656fff260.

--- .travis-ci/deploy-docker.sh
+++ .travis-ci/deploy-docker.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
 if echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin; then
-#        docker push $DOCKER_USERNAME/$REPO
+	docker push $DOCKER_USERNAME/$REPO
         docker push $DOCKER_USERNAME/blog
 fi
 
