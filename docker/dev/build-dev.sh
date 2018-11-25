3ea772be6a5b614771708cab2c760dbcc251bad9
Shylock-Hg <tcath2s@gmail.com>
2018-11-25 10:31:32 +0000

    Correct using yay for installation:   1.  Add installation of yay.   2.  Using yay for other installation

--- docker/dev/build-dev.sh
+++ docker/dev/build-dev.sh
@@ -7,8 +7,11 @@
 #   \email tcath2s@gmail.com
 ###############################################################################
 
+# yay
+eval git clone https://aur.archlinux.org/yay.git && \
+cd yay && makepkg --noconfirm -si && cd .. && rm -rf yay .cache && \
 # Archlinux installation
-eval $NATIVE_INSTALL \
+        $AUR_INSTALL \
         base-devel \
         git vi vim emacs-nox gdb clang lldb cmake \
         openssh boost boost-libs valgrind man man-pages \
@@ -18,23 +21,23 @@ eval $NATIVE_INSTALL \
         arm-none-eabi-newlib arm-none-eabi-binutils && \
 # Git configuration
 git config --global user.name 'Shylock-Hg' && \
-git config --global user.email 'tcath2s@gmail.com' \
+git config --global user.email 'tcath2s@gmail.com' && \
 # Conda and torch, tensorflow
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh 2>/dev/null && \
-        sh Miniconda3-latest-Linux-x86_64.sh -b -p ./Miniconda3 && \
-        rm Miniconda3-latest-Linux-x86_64.sh && \
-        conda create --name=ml && \
-        conda install --name=ml tensorflow-gpu && \
-        conda install --name=ml -c pytorch pytorch torchvision cuda92 && \
-        yes y | conda clean --all && \
+sh Miniconda3-latest-Linux-x86_64.sh -b -p ./Miniconda3 && \
+rm Miniconda3-latest-Linux-x86_64.sh && \
+conda create --name=ml && \
+conda install --name=ml tensorflow-gpu && \
+conda install --name=ml -c pytorch pytorch torchvision cuda92 && \
+yes y | conda clean --all && \
 # boudica development environment
-pip2 install --no-cache-dir --user pyyaml && ${NATIVE_INSTALL} scons && \
+pip2 install --no-cache-dir --user pyyaml && $AUR_INSTALL scons && \
 # rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y && \
 # zsh installation & oh-my-zsh configuration
-${NATIVE_INSTALL} zsh powerline-fonts && \
-        sudo chsh -s $(which zsh) shylock && \
-        curl -s https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh && \
+$AUR_INSTALL zsh powerline-fonts && \
+sudo chsh -s $(which zsh) shylock && \
+curl -s https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh && \
 # travis-ci cli
 gem install travis -v 1.8.9 --no-rdoc --no-ri && \
 # Vim installation and configuration
@@ -43,7 +46,7 @@ curl https://raw.githubusercontent.com/Shylock-Hg/config.linux/master/build-vim.
 curl https://raw.githubusercontent.com/Shylock-Hg/config.linux/master/build.sh | sh
 
 # mysql installation and config
-# eval ${NATIVE_INSTALL} mariadb \
+# eval ${AUR_INSTALL} mariadb \
 #        && sudo usermod -a -G mysql shylock \
 #        && sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
