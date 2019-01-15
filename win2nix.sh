4323d01edae029faa8a4e875b9bd7519755158f0
Shylock Hg <tcath2s@gmail.com>
2019-01-15 19:25:28 +0800

    Optimize the win2nix.sh script. (#69)
    1. Adding the directory applied command line argument.
    	2. Detete the trailing spaces of all files in the directory applied.

--- win2nix.sh
+++ win2nix.sh
@@ -4,8 +4,34 @@
 # convert sources(c/c++) from windows to unix flavor
 ###############################################################################
 
-# convert backslash to slash in include path
-# convert line break to unix flavor
-find $PWD -type f -name '*.h' -print -o -name '*.c' -print |\
-xargs sed -i -e '/#include/s/\\/\//g' -e 's/\r//'
 
+dir=$PWD  # default to current dirctory
+
+while getopts 'd:h' args
+do
+	case $args in
+		d)  #!< name of source file
+            dir=$OPTARG
+            if [ ! -d "$dir" ]; then  # not exists directory
+                echo "Invalid directory $dir"
+                exit 1
+            fi
+			;;
+		h)
+                        printf "usage: $0 -d <directory-to-format>\neg.$0 -d ./project\n"
+                        printf "usage: $0 , for current directory!\n"
+			;;
+		?)
+			echo "error:Unknown argument $OPTARG!"
+			;;
+	esac
+done
+
+shift $((OPTIND - 1))
+
+# convert backslash to slash in include path for the c header/source file
+find $dir -type f -name '*.h' -name '*.c' -exec sed -i -e '/#include/s/\\/\//' {} \;
+
+# convert line break to unix flavor
+# delete trailing spaces
+find $dir -type f -exec sed -i -e 's/\r//' -e 's/[[:blank:]]*$//' {} \;
