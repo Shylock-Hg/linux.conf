5c7082636c9c8bcfe83faa58ea92a5a7a3ae8e2a
Shylock Hg <tcath2s@icloud.com>
2024-03-19 20:31:41 +0800

    Fix rust setup.

--- setup-rust.sh
+++ setup-rust.sh
@@ -4,13 +4,21 @@
 
 set -e
 
-export RUSTUP_UPDATE_ROOT=https://mirrors.aliyun.com/rustup/rustup
-export RUSTUP_DIST_SERVER=https://mirrors.aliyun.com/rustup
+export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-echo 'export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static' >>  ~/.zshenv
-echo 'export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup' >> ~/.zshenv
-source ~/.zshenv
+if [ -f '~/.zshenv' ]; then
+    echo "export RUSTUP_UPDATE_ROOT=${RUSTUP_UPDATE_ROOT}" >> ~/.zshenv
+    echo "export RUSTUP_DIST_SERVER=${RUSTUP_DIST_SERVER}" >>  ~/.zshenv
+fi
+
+
+
+if [ -f '~/.bashrc' ]; then
+    echo "export RUSTUP_UPDATE_ROOT=${RUSTUP_UPDATE_ROOT}" >> ~/.bashrc
+    echo "export RUSTUP_DIST_SERVER=${RUSTUP_DIST_SERVER}" >>  ~/.bashrc
+fi
 
 cp .cargo.conf  ~/.cargo/config
