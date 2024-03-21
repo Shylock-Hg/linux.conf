5e98600837b62486d1e825cd67200f1583540e12
Shylock Hg <shylock@localhost.localdomain>
2024-03-21 17:25:12 +0800

    Fix.

--- fix-ms-edge-cjk-display.sh
+++ fix-ms-edge-cjk-display.sh
@@ -3,4 +3,5 @@
 # Yes, it woks!
 # And later, run edge by icon is ok too.
 # But maybe need to install cjk fonts first.
+# Maybe need a reboot.
 /etc/alternatives/microsoft-edge --enable-logging --v=1 zhihu.com
